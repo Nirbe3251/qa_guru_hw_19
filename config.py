aef57a4a937df6ea3f66dde9dce21eee4af92305
Daniil Efimow <daniil.efimow@mail.ru>
2025-08-09 14:55:24 +0300

    add mobile test`s

---
+++ config.py
@@ -0,0 +1,5 @@
+import os
+
+context = os.getenv('context', 'bstack')
+bstack_userName = os.getenv('bstack_userName', 'bsuser_8dVKyt')
+bstack_accessKey = os.getenv('bstack_accessKey', 'opfSXq6sxqs55TSvN6iX')
---
+++ tests/test_android.py
@@ -0,0 +1,27 @@
+from allure_commons._allure import step
+from appium.webdriver.common.appiumby import AppiumBy
+from selene import browser, have
+
+
+def test_search():
+    with step('Type search'):
+        browser.element((AppiumBy.ACCESSIBILITY_ID, "Search Wikipedia")).click()
+        browser.element((AppiumBy.ID, "org.wikipedia.alpha:id/search_src_text")).type('Appium')
+
+    with step('Verify content found'):
+        results = browser.all((AppiumBy.ID, 'org.wikipedia.alpha:id/page_list_item_title'))
+        results.should(have.size_greater_than(0))
+        results.first.should(have.text('Appium'))
+
+def test_search_wikipedia_bird():
+    with step('Type search in Wikipedia'):
+        browser.element((AppiumBy.ACCESSIBILITY_ID, "Search Wikipedia")).click()
+        browser.element((AppiumBy.ID, "org.wikipedia.alpha:id/search_src_text")).type('Bird')
+
+    with step('Verify content found'):
+        results = browser.all((AppiumBy.ID, 'org.wikipedia.alpha:id/page_list_item_title'))
+        results.should(have.size_greater_than(0))
+        results.first.should(have.text('Bird'))
+
+    with step('Click an item'):
+        browser.all((AppiumBy.ID, 'org.wikipedia.alpha:id/page_list_item_title')).first.click()
